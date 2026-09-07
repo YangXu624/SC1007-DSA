78e8e8c6b49373762e30a552c55e58827f466eef
YangXu624 <yangxu624@gmail.com>
2026-09-07 12:54:47 +0800

    completed tutorial 5 q4 quite difficult

--- tutorials/tut5/prefixInfixPostix.py
+++ tutorials/tut5/prefixInfixPostix.py
@@ -6,3 +6,57 @@ PRECEDENCE = {
     "=": 1
 }
 
+def inToPost(tokens):
+    stack = []
+    new_expression = []
+    for token in tokens:
+        if token == "(":
+            stack.append(token)
+        elif token == ")":
+            while stack and stack[-1] != "(":
+                new_expression.append(stack.pop()) # pop until reach other bracket
+            if stack:
+                stack.pop() # throw away bracket
+        elif token in PRECEDENCE:
+            while stack and stack[-1] != "(" and PRECEDENCE[token] <= PRECEDENCE[stack[-1]]: # if incoming higher than top of stack
+                new_expression.append(stack.pop()) # pop all that are necessary
+            stack.append(token)
+        else:
+            new_expression.append(token) # for numbers
+
+    while stack:
+        new_expression.append(stack.pop())
+
+    return new_expression
+
+
+def preToIn(tokens):
+    stack = []
+    for token in tokens[::-1]: # go right to left
+        if token not in PRECEDENCE: # if token is operand
+            stack.append(token)
+        else: # if token is operator
+            left_op = stack.pop()
+            right_op = stack.pop()
+            stack.append(f"( {left_op} {token} {right_op} )") # push as a new operand
+    return stack
+
+
+def postToPre(tokens):
+    stack = []
+    for token in tokens:
+        if token not in PRECEDENCE:
+            stack.append(token) # push to stack if number
+        else: # if token is operator, pop 2 operands
+            right_op = stack.pop()
+            left_op = stack.pop()
+            stack.append(f"{token} {left_op} {right_op}") # push as new operand
+    return stack
+
+if __name__ == "__main__":
+    expression = input("Enter the expression: ")
+    tokens = expression.split()
+    # new_expression = inToPost(tokens)
+    # new_expression = preToIn(tokens)
+    new_expression = postToPre(tokens)
+    print(" ".join(new_expression))
